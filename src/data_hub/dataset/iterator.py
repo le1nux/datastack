4feb0b3426462daaf4c3134ecec9115353a90ba4
Max Lübbering <max.luebbering@gmail.com>
2020-08-22 21:52:36 +0200

    Storing datasetname now in the iterator itself

--- src/data_hub/dataset/iterator.py
+++ src/data_hub/dataset/iterator.py
@@ -2,6 +2,10 @@ from abc import ABC, abstractmethod
 
 
 class DatasetIteratorIF(ABC):
+
+    def __init__(self, dataset_name: str):
+        self._dataset_name = dataset_name
+
     @abstractmethod
     def __len__(self):
         raise NotImplementedError
@@ -9,3 +13,7 @@ class DatasetIteratorIF(ABC):
     @abstractmethod
     def __getitem__(self, index: int):
         raise NotImplementedError
+
+    @property
+    def dataset_name(self) -> str:
+        return self._dataset_name
--- src/data_hub/mnist/iterator.py
+++ src/data_hub/mnist/iterator.py
@@ -1,13 +1,14 @@
 import torch
-from typing import List
 from data_hub.io.resources import StreamedResource
+from data_hub.dataset.iterator import DatasetIteratorIF
 
 
-class MNISTIterator:
+class MNISTIterator(DatasetIteratorIF):
     """ MNIST dataset iterator (http://yann.lecun.com/exdb/mnist/)
     """
 
     def __init__(self, samples_stream: StreamedResource, targets_stream: StreamedResource = None):
+        super().__init__(dataset_name="MNIST")
         self.samples = torch.load(samples_stream)
         self.targets = torch.load(targets_stream)
 
