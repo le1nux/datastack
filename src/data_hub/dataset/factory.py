3dddeda8eb7857124bf6754180a5d2c374b8a24b
Max Lübbering <max.luebbering@gmail.com>
2020-10-26 15:06:00 +0100

    added sample_pos, target_pos and tag_pos properties to iterators

--- src/data_hub/dataset/factory.py
+++ src/data_hub/dataset/factory.py
@@ -1,7 +1,6 @@
 from abc import ABC
 from data_hub.io.storage_connectors import StorageConnector
 from data_hub.dataset.iterator import DatasetIteratorIF
-from dataclasses import dataclass
 
 
 class DatasetFactory(ABC):
--- src/data_hub/dataset/iterator.py
+++ src/data_hub/dataset/iterator.py
@@ -22,13 +22,36 @@ class DatasetIteratorIF(ABC):
     def dataset_tag(self) -> str:
         raise NotImplementedError
 
+    @property
+    @abstractmethod
+    def underlying_iterators(self) -> List["DatasetIteratorIF"]:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def sample_pos(self) -> int:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def target_pos(self) -> int:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def tag_pos(self) -> int:
+        raise NotImplementedError
+
 
 class DatasetIterator(DatasetIteratorIF):
 
-    def __init__(self, dataset_sequences: List[Sequence], dataset_name: str = None, dataset_tag: str = None):
+    def __init__(self, dataset_sequences: List[Sequence], dataset_name: str = None, dataset_tag: str = None, sample_pos: int = 0, target_pos: int = 1, tag_pos: int = 2):
         self._dataset_name = dataset_name
         self._dataset_sequences = dataset_sequences
         self._dataset_tag = dataset_tag
+        self._sample_pos = sample_pos
+        self._target_pos = target_pos
+        self._tag_pos = tag_pos
 
     def __len__(self):
         return len(self._dataset_sequences[0])
@@ -43,6 +66,22 @@ class DatasetIterator(DatasetIteratorIF):
     @property
     def dataset_tag(self) -> str:
         return self._dataset_tag
+
+    @property
+    def underlying_iterators(self) -> List["DatasetIteratorIF"]:
+        return []
+
+    @property
+    def sample_pos(self) -> int:
+        return self._sample_pos
+
+    @property
+    def target_pos(self) -> int:
+        return self._target_pos
+
+    @property
+    def tag_pos(self) -> int:
+        return self._tag_pos
 
 
 class DatasetIteratorView(DatasetIteratorIF):
@@ -67,6 +106,22 @@ class DatasetIteratorView(DatasetIteratorIF):
     @property
     def dataset_tag(self) -> str:
         return self._dataset_tag
+
+    @property
+    def underlying_iterators(self) -> List["DatasetIteratorIF"]:
+        return [self._dataset_iterator]
+
+    @property
+    def sample_pos(self) -> int:
+        return self._dataset_iterator.sample_pos
+
+    @property
+    def target_pos(self) -> int:
+        return self._dataset_iterator.target_pos
+
+    @property
+    def tag_pos(self) -> int:
+        return self._dataset_iterator.tag_pos
 
 
 class CombinedDatasetIterator(DatasetIteratorIF):
@@ -95,3 +150,19 @@ class CombinedDatasetIterator(DatasetIteratorIF):
     @property
     def dataset_tag(self) -> str:
         return self._dataset_tag
+
+    @property
+    def underlying_iterators(self) -> List["DatasetIteratorIF"]:
+        return self._iterators
+
+    @property
+    def sample_pos(self) -> int:
+        return self._iterators[0].sample_pos
+
+    @property
+    def target_pos(self) -> int:
+        return self._iterators[0].target_pos
+
+    @property
+    def tag_pos(self) -> int:
+        return self._iterators[0].tag_pos
