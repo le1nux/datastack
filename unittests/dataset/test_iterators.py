157bfb533cb321db980b8343b11fd387f88d589b
Max Lübbering <max.luebbering@gmail.com>
2020-10-26 15:03:58 +0100

    added tests on iterators

--- unittests/dataset/test_iterators.py
+++ unittests/dataset/test_iterators.py
@@ -1,45 +1,70 @@
 import pytest
 from typing import List, Dict, Any, Tuple
-from data_hub.dataset.iterator import DatasetIteratorIF
+from data_hub.dataset.iterator import DatasetIteratorIF, DatasetIterator, DatasetIteratorView, CombinedDatasetIterator
+from itertools import chain
 
 
-class TestLabelMapper:
+class TestIterator:
     @pytest.fixture
     def target_position(self) -> int:
         return 1
 
-    # @pytest.fixture
-    # def label_mapper(self, target_position: int) -> Dict[str, Any]:
-    #     mappings = [
-    #         {
-    #             "previous_labels": [0, 1, 2],
-    #             "new_label": 0
-    #         },
-    #         {
-    #             "previous_labels": [3],
-    #             "new_label": 1
-    #         },
-    #     ]
-    #     label_mapper = LabelMapper(mappings, target_position)
-    #     return label_mapper
-
     @pytest.fixture
-    def samples(self) -> List[Tuple[Any]]:
-        # returns tuples of format (sample, target)
+    def sequences(self) -> List[Tuple[Any]]:
+        # returns tuples of format [sample_sequence, target_sequence, tag_sequence]
         return [
-            ([1, 2, 3], 1),
-            ([3, 2, 2], 0),
-            ([2, 2, 6], 2),
-            ([5, 2, 5], 3),
-            ([1, 5, 3], 3)
+            [(1, 2, 3), (3, 2, 2), (2, 2, 6), (5, 2, 5), (1, 5, 3)],
+            [1, 0, 2, 3, 3],
+            ["b", "a", "c", "d", "d"]
         ]
 
     @pytest.fixture
-    def dataset_iterator(self, samples) -> DatasetIteratorIF:
-        return [sample for sample in samples]
+    def dataset_iterator(self, sequences) -> DatasetIteratorIF:
+        return DatasetIterator(dataset_sequences=sequences,
+                               dataset_name="TEST DATASET",
+                               dataset_tag="train",
+                               sample_pos=0,
+                               target_pos=1,
+                               tag_pos=2)
 
-    # def test_post_proceessed_dataset_iterator(self, dataset_iterator: DatasetIteratorIF, label_mapper: LabelMapper, target_position: int, samples: List[Tuple[Any]]):
-    #     iterator = PostProcessedDatasetIterator(dataset_iterator, label_mapper)
-    #     targets = [0, 0, 0, 1, 1]
-    #     assert all([sample[target_position] == targets[i] for i, sample in enumerate(iterator)])
-    #     assert all([sample[0] == samples[i][0] for i, sample in enumerate(iterator)])
+    @pytest.fixture
+    def dataset_view_indices(self) -> List[int]:
+        return [0, 2]
+
+    @pytest.fixture
+    def dataset_iterator_view(self, dataset_iterator, dataset_view_indices) -> DatasetIteratorIF:
+        return DatasetIteratorView(dataset_iterator, indices=dataset_view_indices, dataset_tag="bla")
+
+    def test_dataset_iterator(self, dataset_iterator: DatasetIteratorIF, sequences):
+        for orig_sample, iterator_sample in zip(zip(*sequences), dataset_iterator):
+            assert orig_sample[dataset_iterator.sample_pos] == iterator_sample[dataset_iterator.sample_pos]
+            assert orig_sample[dataset_iterator.target_pos] == iterator_sample[dataset_iterator.target_pos]
+            assert orig_sample[dataset_iterator.tag_pos] == iterator_sample[dataset_iterator.tag_pos]
+
+    def test_dataset_iterator_view(self, dataset_iterator_view: DatasetIteratorIF, dataset_view_indices, sequences):
+        for orig_sample, iterator_sample in zip([[s[i] for s in sequences] for i in dataset_view_indices], dataset_iterator_view):
+            assert orig_sample[dataset_iterator_view.sample_pos] == iterator_sample[dataset_iterator_view.sample_pos]
+            assert orig_sample[dataset_iterator_view.target_pos] == iterator_sample[dataset_iterator_view.target_pos]
+            assert orig_sample[dataset_iterator_view.tag_pos] == iterator_sample[dataset_iterator_view.tag_pos]
+
+    def test_dataset_iterator_combined(self, sequences):
+        iterator_1 = DatasetIterator(dataset_sequences=sequences,
+                                     dataset_name="TEST DATASET",
+                                     dataset_tag="train",
+                                     sample_pos=0,
+                                     target_pos=1,
+                                     tag_pos=2)
+
+        iterator_2 = DatasetIterator(dataset_sequences=sequences,
+                                     dataset_name="TEST DATASET",
+                                     dataset_tag="train",
+                                     sample_pos=0,
+                                     target_pos=1,
+                                     tag_pos=2)
+        sequences_combined = chain(sequences, sequences)
+        combined_iterator = CombinedDatasetIterator(iterators=[iterator_1, iterator_2], dataset_name="combined", dataset_tag="abc")
+
+        for orig_sample, iterator_sample in zip(zip(*sequences_combined), combined_iterator):
+            assert orig_sample[combined_iterator.sample_pos] == iterator_sample[combined_iterator.sample_pos]
+            assert orig_sample[combined_iterator.target_pos] == iterator_sample[combined_iterator.target_pos]
+            assert orig_sample[combined_iterator.tag_pos] == iterator_sample[combined_iterator.tag_pos]
