0b0e538fefc00c22e5930ae4e94b306470d54917
Max Lübbering <max.luebbering@gmail.com>
2020-10-26 21:17:30 +0100

    added missing dependency

--- src/setup.py
+++ src/setup.py
@@ -7,6 +7,7 @@ setup(
     install_requires=["torch",
                       "torchvision",
                       "tqdm",
+                      "yaml"
                       ],
     python_requires=">=3.7"
 )
