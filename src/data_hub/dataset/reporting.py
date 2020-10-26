5f44f920ddd5520b2d66b65d9d864e4ce2117c88
Max Lübbering <max.luebbering@gmail.com>
2020-10-26 20:42:08 +0100

    implemented reporting of dataset iterators

---
+++ src/data_hub/dataset/reporting.py
@@ -0,0 +1,93 @@
+from data_hub.dataset.iterator import DatasetIteratorIF
+from dataclasses import dataclass
+from typing import List, Union, Dict, Any
+from collections import Counter
+import time
+from enum import Enum
+import json
+import yaml
+import dataclasses
+
+
+@dataclass
+class DatasetIteratorReport:
+    name: str
+    tag: str
+    length: int
+    sample_pos: int
+    target_pos: int
+    tag_pos: int
+    sample_shape: List[int]
+    target_dist: Dict[Union[str, int], int]
+    iteration_speed: float
+    sub_reports: List["DatasetIteratorReport"]
+
+
+class DatasetIteratorReportGenerator:
+    class ReportFormat(Enum):
+        DATA_CLASS = "data_class"
+        YAML = "yaml"
+        JSON = "json"
+        DICT = "dict"
+
+    def generate_report(iterator: DatasetIteratorIF, report_format: ReportFormat = ReportFormat.DATA_CLASS):
+        sub_reports = [DatasetIteratorReportGenerator.generate_report(sub_iterator) for sub_iterator in iterator.underlying_iterators]
+        target_dist = {k: v for k, v in sorted(Counter([row[iterator.target_pos] for row in iterator]).items())}
+        iteration_speed = DatasetIteratorReportGenerator.measure_iteration_speed(iterator)
+        report = DatasetIteratorReport(iterator.dataset_name, iterator.dataset_tag, len(iterator), iterator.sample_pos, iterator.target_pos,
+                                       iterator.tag_pos, iterator[0][iterator.sample_pos].shape, target_dist, iteration_speed, sub_reports)
+        if report_format == DatasetIteratorReportGenerator.ReportFormat.JSON:
+            return DatasetIteratorReportGenerator._to_json(report)
+        elif report_format == DatasetIteratorReportGenerator.ReportFormat.YAML:
+            return DatasetIteratorReportGenerator._to_yaml(report)
+        elif report_format == DatasetIteratorReportGenerator.ReportFormat.DICT:
+            return DatasetIteratorReportGenerator._to_dict(report)
+        else:
+            return report
+
+    def measure_iteration_speed(iterator: DatasetIteratorIF) -> float:
+        def full_iteration(iterator: DatasetIteratorIF):
+            for _ in iterator:
+                pass
+
+        diff = 0
+        iterations = 0
+        while(diff < 10):
+            start = time.time()
+            full_iteration(iterator)
+            end = time.time()
+            diff += end-start
+            iterations += 1
+        return len(iterator)*iterations / diff  # iterations per second
+
+    def _to_json(report: DatasetIteratorReport) -> str:
+        return json.dumps(dataclasses.asdict(report), sort_keys=True, indent=4)
+
+    def _to_yaml(report: DatasetIteratorReport) -> str:
+        return yaml.dump(dataclasses.asdict(report), sort_keys=True, indent=4)
+
+    def _to_dict(report: DatasetIteratorReport) -> Dict[str, Any]:
+        return dataclasses.asdict(report)
+
+
+if __name__ == "__main__":
+    from data_hub.mnist.factory import MNISTFactory
+    import data_hub
+    import os
+    from data_hub.io.storage_connectors import FileStorageConnector
+
+    data_hub_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(data_hub.__file__))))
+    example_file_storage_path = os.path.join(data_hub_root, "example_file_storage")
+    storage_connector = FileStorageConnector(root_path=example_file_storage_path)
+    mnist_factory = MNISTFactory(storage_connector)
+    iterator = mnist_factory.get_dataset_iterator(split="train")
+    report = DatasetIteratorReportGenerator.generate_report(iterator)
+    print(report)
+
+    from data_hub.dataset.iterator import CombinedDatasetIterator
+    iterator_train = mnist_factory.get_dataset_iterator(split="train")
+    iterator_test = mnist_factory.get_dataset_iterator(split="test")
+
+    iterator = CombinedDatasetIterator([iterator_train, iterator_train, iterator_train], "combined", "my_tag")
+    report = DatasetIteratorReportGenerator.generate_report(iterator)
+    print(report)
