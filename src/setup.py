67793bf9a58f7339c24d4bbdbae6ad51550d348f
Max Lübbering <max.luebbering@gmail.com>
2020-11-23 22:01:11 +0100

    added dependencies

--- src/setup.py
+++ src/setup.py
@@ -5,13 +5,16 @@ with open("../README.md", "r") as fh:
 
 setup(
     name='datastack',
-    version='0.0.1',
+    version='0.0.2',
     author='Max Luebbering',
     description="DataStack, a stream based solution for machine learning dataset retrieval and storage",
     long_description=long_description,
     url="https://github.com/le1nux/datastack",
     packages=find_packages(),
-    install_requires=["torch",
+    install_requires=[
+                      "pytest",
+                      "pytest-cov",
+                      "torch",
                       "torchvision",
                       "tqdm",
                       "PyYAML"
