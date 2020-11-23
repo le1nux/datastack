ccdc6a67cc17d9ec77f0236d2863d7fc65d274ec
Max Lübbering <max.luebbering@gmail.com>
2020-11-23 21:01:22 +0100

    fixed leaking fd in MNIST Iterator

--- src/data_stack/mnist/iterator.py
+++ src/data_stack/mnist/iterator.py
@@ -10,4 +10,5 @@ class MNISTIterator(SequenceDatasetIterator):
     def __init__(self, samples_stream: StreamedResource, targets_stream: StreamedResource):
         targets = [int(target) for target in torch.load(targets_stream)]
         dataset_sequences = [torch.load(samples_stream), targets]
+        samples_stream.close()
         super().__init__(dataset_sequences=dataset_sequences)
