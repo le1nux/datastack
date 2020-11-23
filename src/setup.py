506c7b327271a00cf645173c254ce83a6cbe6c1f
Max Lübbering <max.luebbering@gmail.com>
2020-11-23 21:01:40 +0100

    added long description to setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,10 +1,14 @@
 from setuptools import find_packages, setup
 
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name='data_stack',
+    name='datastack',
     version='0.0.1',
     author='Max Luebbering',
     description="DataStack, a stream based solution for machine learning dataset retrieval and storage",
+    long_description=long_description,
     url="https://github.com/le1nux/datastack",
     packages=find_packages(),
     install_requires=["torch",
