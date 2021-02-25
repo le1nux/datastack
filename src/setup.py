b02376091b023e7653335378dbcec1e5d33d25d1
Max Lübbering <le1nux@users.noreply.github.com>
2021-02-25 13:38:13 +0100

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ with open("../README.md", "r") as fh:
 
 setup(
     name='datastack',
-    version='0.0.7',
+    version='0.0.8',
     author='Max Luebbering',
     description="DataStack, a stream based solution for machine learning dataset retrieval and storage",
     long_description=long_description,
