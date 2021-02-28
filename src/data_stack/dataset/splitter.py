e9041e6168b04f5d5123bfea8d6e521b67d8cadc
Max Lübbering <max.luebbering@gmail.com>
2021-02-28 13:33:09 +0100

    implemented NestedCVSplitterImpl

--- src/data_stack/dataset/splitter.py
+++ src/data_stack/dataset/splitter.py
@@ -1,7 +1,9 @@
 from data_stack.dataset.iterator import DatasetIteratorIF, DatasetIteratorView
-from typing import List
+from typing import List, Tuple
 from abc import ABC, abstractmethod
 import random
+from sklearn.model_selection import StratifiedKFold, KFold, GroupKFold
+import numpy as np
 
 
 class SplitterFactory:
@@ -55,3 +57,42 @@ class RandomSplitterImpl(SplitterIF):
         # if we don't have a round split, we add the remaining samples to the last split.
         split_indices[-1] = split_indices[-1] + indices[upper:]
         return split_indices
+
+
+class NestedCVSplitterImpl(SplitterIF):
+
+    def __init__(self,
+                 num_outer_loop_folds: int = 5,
+                 num_inner_loop_folds: int = 2,
+                 inner_stratification: bool = True,
+                 outer_stratification: bool = True,
+                 target_pos: int = 1,
+                 shuffle: bool = True,
+                 seed: int = 1):
+        self.num_outer_loop_folds = num_outer_loop_folds
+        self.num_inner_loop_folds = num_inner_loop_folds
+        self.random_state = np.random.RandomState(seed=seed)
+        self.target_pos = target_pos
+
+        if inner_stratification:
+            self.inner_splitter = StratifiedKFold(n_splits=num_inner_loop_folds, shuffle=shuffle, random_state=self.random_state)
+        else:
+            self.inner_splitter = KFold(n_splits=num_inner_loop_folds, shuffle=shuffle, random_state=self.random_state)
+        if outer_stratification:
+            self.outer_splitter = StratifiedKFold(n_splits=num_outer_loop_folds, shuffle=shuffle, random_state=self.random_state)
+        else:
+            self.outer_splitter = KFold(n_splits=num_outer_loop_folds, shuffle=shuffle, random_state=self.random_state)
+
+    def split(self, dataset_iterator: DatasetIteratorIF) -> Tuple[List[DatasetIteratorIF], List[List[DatasetIteratorIF]]]:
+        # create outer loop folds
+        targets = [sample[self.target_pos] for sample in dataset_iterator]
+        folds_indices = [fold[1] for fold in self.outer_splitter.split(X=np.zeros(len(targets)), y=targets)]
+        outer_folds = [DatasetIteratorView(dataset_iterator, fold_indices) for fold_indices in folds_indices]
+        # create inner loop folds
+        inner_folds_list = []  # contains [inner folds of outer_fold_1, inner folds of outer_fold_2 ...]
+        for iterator in outer_folds:
+            targets = [sample[self.target_pos] for sample in iterator]
+            folds_indices = [fold[1] for fold in self.inner_splitter.split(X=np.zeros(len(targets)), y=targets)]
+            inner_folds = [DatasetIteratorView(iterator, fold_indices) for fold_indices in folds_indices]
+            inner_folds_list.append(inner_folds)
+        return outer_folds, inner_folds_list
